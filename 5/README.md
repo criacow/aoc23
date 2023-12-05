e66202466475ff9498c0cd7534f068ec17fd99a7
Cow <criacow@gmail.com>
2023-12-05 16:50:16 +0000

    day 5

---
+++ 5/README.md
@@ -0,0 +1,7 @@
+a note on the solution:
+
+part 2 wasn't hard code wise but required billions of iterations, the way i wrote it, and was never going to finish
+
+did i rearchitect and optimize my code? haha. did i plonk down 80c/hr for a c7a.2xl, parallelize it over 16 cores, and go to bed?
+
+![five hours between submissions](5-timers.png)
---
+++ 5/seedmap2.pl
@@ -0,0 +1,155 @@
+#!/usr/bin/perl
+
+use strict;
+
+my @seeds;
+my @seed_soil_map;
+my @soil_fert_map;
+my @fert_watr_map;
+my @watr_ligh_map;
+my @ligh_temp_map;
+my @temp_humi_map;
+my @humi_loca_map;
+
+  # seeds
+  @seeds = split(" ", <>);
+  my $skip = <>;
+
+  # seed to soil
+  my $skip = <>;
+  while (<>) {
+    if ($_ eq "\n") { last; }
+    my @dsr = split(" ", $_);
+    push @seed_soil_map, [ @dsr ];
+  }
+
+  my $skip = <>;
+  while (<>) {
+    if ($_ eq "\n") { last; }
+    my @dsr = split(" ", $_);
+    push @soil_fert_map, [ @dsr ];
+  }
+
+  my $skip = <>;
+  while (<>) {
+    if ($_ eq "\n") { last; }
+    my @dsr = split(" ", $_);
+    push @fert_watr_map, [ @dsr ];
+  }
+
+  my $skip = <>;
+  while (<>) {
+    if ($_ eq "\n") { last; }
+    my @dsr = split(" ", $_);
+    push @watr_ligh_map, [ @dsr ];
+  }
+
+  my $skip = <>;
+  while (<>) {
+    if ($_ eq "\n") { last; }
+    my @dsr = split(" ", $_);
+    push @ligh_temp_map, [ @dsr ];
+  }
+
+  my $skip = <>;
+  while (<>) {
+    if ($_ eq "\n") { last; }
+    my @dsr = split(" ", $_);
+    push @temp_humi_map, [ @dsr ];
+  }
+
+  my $skip = <>;
+  while (<>) {
+    if ($_ eq "\n") { last; }
+    my @dsr = split(" ", $_);
+    push @humi_loca_map, [ @dsr ];
+  }
+
+  my $location = -1;
+
+shift(@seeds);
+for (my $snum = 0; $snum < @seeds; $snum += 2) {
+  my $start = $seeds[$snum];
+  my $range = $seeds[$snum + 1];
+  for my $seed ($start .. $start + $range - 1) {
+    my $soil = -1;
+    for my $dsr ( @seed_soil_map ) {
+      if (($seed >= @$dsr[1]) && ($seed < (@$dsr[1] + @$dsr[2]))) {
+        $soil = $seed - @$dsr[1] + @$dsr[0];
+      }
+    }
+    if ($soil == -1) {
+      $soil = $seed;
+    }
+
+    my $fert = -1;
+    for my $dsr ( @soil_fert_map ) {
+      if (($soil >= @$dsr[1]) && ($soil < (@$dsr[1] + @$dsr[2]))) {
+        $fert = $soil - @$dsr[1] + @$dsr[0];
+      }
+    }
+    if ($fert == -1) {
+      $fert = $soil;
+    }
+
+    my $watr = -1;
+    for my $dsr ( @fert_watr_map ) {
+      if (($fert >= @$dsr[1]) && ($fert < (@$dsr[1] + @$dsr[2]))) {
+        $watr = $fert - @$dsr[1] + @$dsr[0];
+      }
+    }
+    if ($watr == -1) {
+      $watr = $fert;
+    }
+
+    my $ligh = -1;
+    for my $dsr ( @watr_ligh_map ) {
+      if (($watr >= @$dsr[1]) && ($watr < (@$dsr[1] + @$dsr[2]))) {
+        $ligh = $watr - @$dsr[1] + @$dsr[0];
+      }
+    }
+    if ($ligh == -1) {
+      $ligh = $watr;
+    }
+
+    my $temp = -1;
+    for my $dsr ( @ligh_temp_map ) {
+      if (($ligh >= @$dsr[1]) && ($ligh < (@$dsr[1] + @$dsr[2]))) {
+        $temp = $ligh - @$dsr[1] + @$dsr[0];
+      }
+    }
+    if ($temp == -1) {
+      $temp = $ligh;
+    }
+
+    my $humi = -1;
+    for my $dsr ( @temp_humi_map ) {
+      if (($temp >= @$dsr[1]) && ($temp < (@$dsr[1] + @$dsr[2]))) {
+        $humi = $temp - @$dsr[1] + @$dsr[0];
+      }
+    }
+    if ($humi == -1) {
+      $humi = $temp;
+    }
+
+    my $loca = -1;
+    for my $dsr ( @humi_loca_map ) {
+      if (($humi >= @$dsr[1]) && ($humi < (@$dsr[1] + @$dsr[2]))) {
+        $loca = $humi - @$dsr[1] + @$dsr[0];
+      }
+    }
+    if ($loca == -1) {
+      $loca = $humi;
+    }
+
+#    print "$seed,$soil,$fert,$watr,$ligh,$temp,$humi,$loca\n";
+    if ($location == -1) {
+      $location = $loca;
+    } elsif ($loca < $location) {
+      $location = $loca;
+    }
+#    print "new location: $location\n";
+  }
+}
+print "$location\n";
+
